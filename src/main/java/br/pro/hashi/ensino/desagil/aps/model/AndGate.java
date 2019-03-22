f290dcb21346a6cca17f0fa0c3555a8f55ea0bab
Pedro Fontes <pvfontes1000@gmailcom>
2019-03-22 11:42:14 -0300

    and gate

---
+++ src/main/java/br/pro/hashi/ensino/desagil/aps/model/AndGate.java
@@ -0,0 +1,41 @@
+package br.pro.hashi.ensino.desagil.aps.model;
+
+public class AndGate extends Gate {
+    private final NandGate nand1;
+    private final NandGate nand2;
+
+    public AndGate(){
+        super(2);
+
+        nand1 = new NandGate();
+        nand2 = new NandGate();
+
+        nand2.connect(0, nand1);
+        nand2.connect(1, nand1);
+
+    }
+
+    @Override
+    public boolean read() {
+        return nand2.read();
+    }
+
+
+    @Override
+    public void connect(int inputPin, SignalEmitter emitter) {
+
+        if (inputPin != 0 && inputPin != 1) {
+            throw new IndexOutOfBoundsException(inputPin);
+        }
+
+        if (inputPin == 1){
+            nand1.connect(1, emitter);
+        }
+
+        if (inputPin == 0){
+            nand1.connect(0, emitter);
+        }
+
+    }
+
+}
